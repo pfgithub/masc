56f82518e3fe0b457e08e7276104180432fb08ee
pfg <omegariisthebest@gmail.com>
2020-05-26 23:57:00 -0700

    disallow a and v as userregisters

--- src/conv.ts
+++ src/conv.ts
@@ -46,8 +46,6 @@ let regExpansions: { [key: string]: string[] } = {
 let userRegisters: string[] = [
     "t0", "t1", "t2", "t3", "t4","t5", "t6", "t7",
     "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
-    "a0", "a1", "a2", "a3",
-    "v0", "v1"
 ];
 
 let matchIRRegisters = /%%:(?:out\:)?register:(..):%%/g;
